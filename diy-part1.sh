1992abeed36b92981dce77973e480d85b25d7f84
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-10-02 13:17:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#无线
+sed -i "s/ssid=OpenWrt/ssid=DAMI/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
