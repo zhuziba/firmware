2ee1c04270fe2561bb902a8c9fa4872f935adf45
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-10-03 15:14:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo '添加rufengsuixing的adguardhome插件'
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 #无线
 sed -i "s/ssid=OpenWrt/ssid=DAMI/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
