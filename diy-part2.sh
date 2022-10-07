cd5956f971e6700e0b0797a713e3e90e373894f2
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-10-07 18:12:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,9 +22,13 @@ sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES 
 #mv temp/v2raya-openwrt/v2raya package/lean/v2raya
 
 #OpenClash
-git clone -b v0.45.59-beta --depth=1 https://github.com/vernesong/OpenClash.git temp/OpenClash
-mv temp/OpenClash/luci-app-openclash package/lean/luci-app-openclash
-#smartdns
+#git clone -b v0.45.59-beta --depth=1 https://github.com/vernesong/OpenClash.git temp/OpenClash
+#mv temp/OpenClash/luci-app-openclash package/lean/luci-app-openclash
+
 git clone https://github.com/pymumu/openwrt-smartdns.git package/diy/smartdns
-#luci-app-smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/diy/luci-app-smartdns
+git clone https://github.com/chandelures/openwrt-clash.git package/diy/openwrt-clash
+git clone https://github.com/chandelures/luci-app-simple-clash.git package/diy/luci-app-simple-clash
+git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/diy/luci-app-dnsfilter 
+git clone https://github.com/destan19/OpenAppFilter.git package/diy/OpenAppFilter
+git clone https://github.com/QiuSimons/openwrt-mos.git package/diy/openwrt-mos
