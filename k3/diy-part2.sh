e6cf8adf2f4fe3d4eb1a946a2a40a6967f47a8b7
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-10-14 01:20:58 +0800

    Update diy-part2.sh

--- k3/diy-part2.sh
+++ k3/diy-part2.sh
@@ -11,8 +11,6 @@
 #
 
 # Modify default IP
-sed -i 's/9625784cf2e4fd9842f1d407681ce4878b5b0dcddbcd31c6135114a30c71e6a8/skip/g' feeds/packages/utils/jq/Makefile
-sed -i 's/9625784cf2e4fd9842f1d407681ce4878b5b0dcddbcd31c6135114a30c71e6a8/skip/g' package/feeds/packages/jq/Makefile
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
 sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3| ; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
