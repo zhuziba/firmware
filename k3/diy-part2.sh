b73a20c5abb76c58e0f6d0fe3e58fdf51ee46a7d
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-10-16 01:05:42 +0800

    Update diy-part2.sh

--- k3/diy-part2.sh
+++ k3/diy-part2.sh
@@ -13,6 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
+sed -i "s#UTC#Asia/Shanghai#" /home/peng/immortalwrt/package/base-files/files/bin/config_generate
 sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3| ; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
 #rm -rf package/feeds/packages/mosdns
 #git clone https://github.com/QiuSimons/openwrt-mos.git package/diy/openwrt-mos
