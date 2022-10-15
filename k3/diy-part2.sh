dc02167f08aab10f92e542eac95e24e97a63162d
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-10-16 00:09:46 +0800

    Update diy-part2.sh

--- k3/diy-part2.sh
+++ k3/diy-part2.sh
@@ -22,6 +22,3 @@ sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES 
 #内核版本大于5.4.156以上的K3的Op无法运行k3screenctrl
 #替换成5.4内核
 sed -i "s/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.4/g" target/linux/bcm53xx/Makefile
-
-rm -rf ./package/firmware/brcmfmac4366c0-firmware-vendor/files/brcmfmac4366c-pcie.bin
-curl -L -H "Cache-Control: no-cache" -o ./package/firmware/brcmfmac4366c0-firmware-vendor/files/brcmfmac4366c-pcie.bin https://cdn.staticaly.com/gh/Hill-98/phicommk3-firmware/master/brcmfmac4366c-pcie.bin.69027
