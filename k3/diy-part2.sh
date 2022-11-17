6ec25d6d596a6d9dabf3e9d2d6431677ad4c7d90
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-11-18 04:49:29 +0800

    Update diy-part2.sh

--- k3/diy-part2.sh
+++ k3/diy-part2.sh
@@ -15,12 +15,6 @@ sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_genera
 sed -i "s/hostname='ImmortalWrt'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
 sed -i "s#UTC#Asia/Shanghai#" package/base-files/files/bin/config_generate
 sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3| ; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
-#rm -rf package/feeds/packages/mosdns
-
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
-#rm -rf package/diy/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
-#sed -i "s#/etc/AdGuardHome.yaml#/etc/config/AdGuardHome.yaml#" package/diy/luci-app-adguardhome/root/etc/config/AdGuardHome
-#echo 'https://download.fastgit.org/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_armv5.tar.gz'>>package/diy/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
 
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav package/diy/luci-app-aliyundrive-webdav
 svn co https://github.com/messense/aliyundrive-fuse/trunk/openwrt/luci-app-aliyundrive-fuse package/diy/luci-app-aliyundrive-fuse
